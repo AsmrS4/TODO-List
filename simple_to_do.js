0059ac21c58cfaf67bc8239304106917c08095a9
Aymir Sunchugashev <as4n.tsu@gmail.com>
2023-09-21 16:12:06 +0700

    добавил небольшой скрипт

--- simple_to_do.js
+++ simple_to_do.js
@@ -1,1 +1,16 @@
-const INPUT_BUTTON = document.getElementById()
+const INPUT_FIELD = document.getElementById("input_row");
+const TODO_LIST = document.getElementById("todo_list");
+
+function addTask(){
+    if(INPUT_FIELD.value === ''){
+        alert("Create a task, please");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = INPUT_FIELD.value;
+        TODO_LIST.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+    }
+    INPUT_FIELD.value = '';
+}
